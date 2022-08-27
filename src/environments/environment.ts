93fc2f7a78536c8ad8f504437d00d35da7cfbeca
CharlesConagher <michaelshy2012@gmail.com>
2022-08-27 17:51:17 +0300

    https fix in environments.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   OpenWeatherCurrentURL: 'https://api.openweathermap.org/data/2.5/weather',
   OpenWeatherDailyURL: 'https://api.openweathermap.org/data/2.5/onecall',
-  OpenWeatherIconsURL: 'http://openweathermap.org/img/wn/',
-  OpenWeatherGeocoderURL: 'http://api.openweathermap.org/geo/1.0/direct',
+  OpenWeatherIconsURL: 'https://openweathermap.org/img/wn/',
+  OpenWeatherGeocoderURL: 'https://api.openweathermap.org/geo/1.0/direct',
   OpenWeatherAPIKey: '61258af22950f7dfdca86ef3a1babe8c'
 };
 
